eed8048262d1281f75ce821f652af0f9350b7ff1
H-ymt <y.handai1272@gmail.com>
2023-10-07 20:33:15 +0900

    add: TOPページを実装する

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
+import Sheet from "./_components/Sheet";
 
 export default function Home() {
   return (
-    <main>
-      <h2 className={styles.title}>採用サイト</h2>
-    </main>
+    <Sheet>
+      <div className={styles.hero}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heroTitle}>
+            Jamstackで高速かつ<span className={styles.break}>セキュアなWebサイトを構築</span>
+          </h1>
+          <p className={styles.heroText}>
+            私たちはモダンなフロントエンド技術とヘッドレスCMSを駆使して、高速かつセキュアでスケーラブルなWebサイトを構築します。
+            あなたのアイデアとスキルで、私たちと一緒に未来を創り上げましょう。
+          </p>
+        </div>
+        <Image className={styles.heroImage} src="/hero.svg" width="556" height="400" alt="" />
+      </div>
+    </Sheet>
   );
 }
