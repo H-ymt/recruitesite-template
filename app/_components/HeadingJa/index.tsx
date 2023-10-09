f520966dc33963520b980875b69e380edcee6847
H-ymt <y.handai1272@gmail.com>
2023-10-09 21:29:28 +0900

    add: タイトルコンポーネントを追加する

---
+++ app/_components/HeadingJa/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.scss";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function HeadingJa({ children }: Props) {
+  return <h1 className={styles.title}>{children}</h1>;
+}
