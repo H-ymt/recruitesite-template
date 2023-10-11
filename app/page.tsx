f709e7e11e3e904856ab7093cf35a96f30879f51
H-ymt <y.handai1272@gmail.com>
2023-10-11 18:13:13 +0900

    add: トップページを実装する

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Sheet from "./_components/Sheet";
+import { PrimaryButton } from "./_components/ButtonLink";
 
 export default function Home() {
   return (
@@ -17,8 +18,37 @@ export default function Home() {
             あなたのアイデアとスキルで、私たちと一緒に未来を創り上げましょう。
           </p>
         </div>
-        <Image className={styles.heroImage} src="/hero.svg" width="556" height="400" alt="" />
+        <Image
+          className={styles.heroImage}
+          src="/hero.svg"
+          width="556"
+          height="400"
+          alt=""
+        />
       </div>
+
+      <section>
+        <h2 className={styles.sectionTitle}>
+          <span className={styles.sectionTitleEn}>New&ensp;graduate</span>
+          新卒採用
+        </h2>
+        <PrimaryButton href="/">もっと見る</PrimaryButton>
+
+        <div className={styles.newGraduateContent}>
+          <div className={styles.newGraduateImage}>
+            <Image
+              src="/new-graduate.svg"
+              alt=""
+              width="1027"
+              height="799"
+              decoding="async"
+            />
+          </div>
+          <p>
+            私たちの会社は、あなたが学んだ知識を活かし、新たな挑戦を通じて成長していく場所です。アイデアと情熱を大切にし、それを活かす機会を大切にします。私たちと一緒に、未来を創造しましょう。
+          </p>
+        </div>
+      </section>
     </Sheet>
   );
 }
