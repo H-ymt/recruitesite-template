69978eb0c4f4d8cf3a044eb73620fe89e6f8ac2d
H-ymt <y.handai1272@gmail.com>
2023-10-11 18:43:09 +0900

    add: セカンダリーボタンを実装する

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -22,3 +22,19 @@ export const PrimaryButton = ({ children, href }: Props) => {
     </>
   );
 };
+
+export const SecondaryButton = ({ children, href }: Props) => {
+  return (
+    <>
+      <Link className={`${styles.button} ${styles.secondaryButton}`} href={href}>
+        {children}
+        <ArrowRightCircle
+          strokeWidth="1.4"
+          size="20"
+          color="#3C4E91"
+          className={styles.icon}
+        />
+      </Link>
+    </>
+  );
+};
