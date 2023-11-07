3740076e0a86f06b3d038a8982fd306933cd8906
H-ymt <y.handai1272@gmail.com>
2023-11-07 18:10:30 +0900

    update: ヘッダーを上部に固定する

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import Menu from "../Menu";
 import styles from "./index.module.scss";
 import Link from "next/link";
 import MenuMobile from "../MenuMobile";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrolled(scrollTop > 0);
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled ? styles.isScrolled : ""}`}>
       <div className={styles.inner}>
         <Link className={styles.logo} href="/">
           <Image src="/favicon.ico" width={32} height={32} alt="" decoding="async" priority />
