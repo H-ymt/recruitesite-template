292e01e6944cc6de00df84b036d8e7529f720c41
H-ymt <y.handai1272@gmail.com>
2023-11-07 10:49:47 +0900

    update: PCメニューをRadix UIで再実装する

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,168 +1,111 @@
 "use client";
 
-import Link from "next/link";
+import { usePathname } from "next/navigation";
+import NextLink from "next/link";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { ChevronDown } from "lucide-react";
 import styles from "./index.module.scss";
-import { Menu as LucideMenu, X, Plus, Minus } from "lucide-react";
-import { useRef, useState } from "react";
+
+type Props = {
+  href: string;
+  children: React.ReactNode;
+};
 
 export default function Menu() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const linkRef = useRef<HTMLDivElement>(null);
-  const childRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
+  const Link = ({ href, ...props }: Props) => {
+    const isActive = pathname === href;
 
-  const toggleIsOpen = () => setIsOpen(!isOpen);
-  const toggleIsMenuOpen = () => setIsMenuOpen(!isMenuOpen);
-
-  const handleFocus = () => {
-    if (childRef.current) {
-      childRef.current.className += ` ${styles.active}`;
-    }
-  };
-
-  const handleBlur = () => {
-    if (childRef.current) {
-      childRef.current.className = childRef.current.className.replace(styles.active, "");
-    }
-  };
-
-  const handleMenuToggle = () => {
-    toggleIsMenuOpen();
-    handleFocus();
+    return (
+      <NextLink href={href} passHref legacyBehavior>
+        <NavigationMenu.Link className={styles.navigationMenuLink} active={isActive} {...props} />
+      </NextLink>
+    );
   };
 
   return (
     <>
-      <button
-        className={styles.button}
-        onClick={toggleIsOpen}
-        aria-label="メニューを開閉する"
-      >
-        {isOpen ? (
-          <X size={36} strokeWidth={1} />
-        ) : (
-          <LucideMenu size={36} strokeWidth={1} />
-        )}
-      </button>
+      <NavigationMenu.Root className={styles.navigationMenuRoot}>
+        <NavigationMenu.List className={styles.navigationMenuList}>
+          <NavigationMenu.Item
+            className={`${styles.navigationMenuItem} ${
+              pathname === "/about" ? styles.isActive : ""
+            }`}
+          >
+            <Link href="/about">企業情報</Link>
+          </NavigationMenu.Item>
 
-      <nav className={`${styles.nav} ${isOpen ? styles.open : ""}`}>
-        <div className={styles.list}>
-          <div className={styles.item}>
-            <Link onClick={toggleIsOpen} className={styles.navLink} href="/about">
-              企業情報
-            </Link>
-          </div>
-
-          <div ref={linkRef} className={`${styles.item} ${styles.parent}`}>
-            <button
-              type="button"
-              className={`${styles.parentInner} ${styles.parentLinkButton}`}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            >
-              <span>職種から探す</span>
-              <Plus
-                size={14}
+          <NavigationMenu.Item className={styles.navigationMenuItem}>
+            <NavigationMenu.Trigger className={styles.navigationMenuTrigger}>
+              職種から探す
+              <ChevronDown
+                size={16}
                 strokeWidth={2}
-                className={`${styles.plusIcon} ${isMenuOpen ? styles.hidden : ""}`}
+                className={styles.chevron}
+                aria-hidden="true"
               />
-              <Minus
-                size={14}
-                strokeWidth={2}
-                className={`${styles.minusIcon} ${isMenuOpen ? "" : styles.hidden}`}
-              />
-            </button>
-
-            <div
-              className={`${styles.parentInner} ${styles.mobile}`}
-              onClick={handleMenuToggle}
-            >
-              <div>職種から探す</div>
-
-              <Plus
-                size={14}
-                strokeWidth={2}
-                className={`${styles.plusIcon} ${isMenuOpen ? styles.hidden : ""}`}
-              />
-              <Minus
-                size={14}
-                strokeWidth={2}
-                className={`${styles.minusIcon} ${isMenuOpen ? "" : styles.hidden}`}
-              />
-            </div>
-
-            <div
-              ref={childRef}
-              className={`${styles.child} ${isMenuOpen ? styles.active : ""}`}
-            >
-              <ul className={styles.childList}>
-                <li className={styles.childItem}>
-                  <Link
-                    onClick={toggleIsOpen}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className={styles.navLink}
-                    href="/position/engineer"
-                  >
-                    エンジニア
-                  </Link>
+            </NavigationMenu.Trigger>
+            <NavigationMenu.Content className={styles.navigationMenuContent}>
+              <ul className={styles.navigationMenuContentList}>
+                <li
+                  className={`${styles.navigationMenuContentItem} ${
+                    pathname === "/position/engineer" ? styles.isActive : ""
+                  }`}
+                >
+                  <Link href="/position/engineer">エンジニア</Link>
                 </li>
-                <li className={styles.childItem}>
-                  <Link
-                    onClick={toggleIsOpen}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className={styles.navLink}
-                    href="/position/designer"
-                  >
-                    Webデザイナー
-                  </Link>
+                <li
+                  className={`${styles.navigationMenuContentItem} ${
+                    pathname === "/position/designer" ? styles.isActive : ""
+                  }`}
+                >
+                  <Link href="/position/designer">Webデザイナー</Link>
                 </li>
-                <li className={styles.childItem}>
-                  <Link
-                    onClick={toggleIsOpen}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className={styles.navLink}
-                    href="/position/customer"
-                  >
-                    カスタマー
-                  </Link>
+                <li
+                  className={`${styles.navigationMenuContentItem} ${
+                    pathname === "/position/customer" ? styles.isActive : ""
+                  }`}
+                >
+                  <Link href="/position/customer">カスタマー</Link>
                 </li>
-                <li className={styles.childItem}>
-                  <Link
-                    onClick={toggleIsOpen}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    className={styles.navLink}
-                    href="/position/crm"
-                  >
-                    CRM
-                  </Link>
+                <li
+                  className={`${styles.navigationMenuContentItem} ${
+                    pathname === "/position/crm" ? styles.isActive : ""
+                  }`}
+                >
+                  <Link href="/position/crm">CRM</Link>
                 </li>
               </ul>
-            </div>
-          </div>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
 
-          <div className={styles.item}>
-            <Link onClick={toggleIsOpen} className={styles.navLink} href="/new-graduates">
-              新卒採用
-            </Link>
-          </div>
+          <NavigationMenu.Item
+            className={`${styles.navigationMenuItem} ${
+              pathname === "/new-graduates" ? styles.isActive : ""
+            }`}
+          >
+            <Link href="/new-graduates">新卒採用</Link>
+          </NavigationMenu.Item>
 
-          <div className={styles.item}>
-            <Link onClick={toggleIsOpen} className={styles.navLink} href="/flow">
-              選考フロー
-            </Link>
-          </div>
+          <NavigationMenu.Item
+            className={`${styles.navigationMenuItem} ${pathname === "/" ? styles.isActive : ""}`}
+          >
+            <Link href="/flow">選考フロー</Link>
+          </NavigationMenu.Item>
 
-          <div className={styles.item}>
-            <Link onClick={toggleIsOpen} className={styles.navLink} href="/interview">
-              社員インタビュー
-            </Link>
-          </div>
+          <NavigationMenu.Item
+            className={`${styles.navigationMenuItem} ${
+              pathname === "/interview" ? styles.isActive : ""
+            }`}
+          >
+            <Link href="/interview">社員インタビュー</Link>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+
+        <div className="ViewportPosition">
+          <NavigationMenu.Viewport className="navigationMenuViewport" />
         </div>
-      </nav>
+      </NavigationMenu.Root>
     </>
   );
 }
