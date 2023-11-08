94837b73c724c5123590124976ceba51713ee1b9
H-ymt <y.handai1272@gmail.com>
2023-11-08 16:45:18 +0900

    feat: 採用ページのサイドナビゲーションコンポーネントを実装する

---
+++ app/_components/SideNavigation/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./index.module.scss";
+
+type Props = {
+  activeId: string;
+  ids: string[];
+  linkTexts: string[];
+};
+
+export default function SideNavigation({ activeId, ids, linkTexts }: Props) {
+  return (
+    <nav className={styles.sideNavigation}>
+      <ul className={styles.sideNavigationList}>
+        {ids.map((id, index) => (
+          <li key={index}>
+            <a
+              href={`#${id}`}
+              className={`${styles.sideMenuLink} ${
+                activeId === id ? styles.isActive : ""
+              }`}
+            >
+              {linkTexts[index]}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
