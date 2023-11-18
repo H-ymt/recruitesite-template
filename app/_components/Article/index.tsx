5e9667e01949e3fa5c0563d8370eec97adbd2734
H-ymt <y.handai1272@gmail.com>
2023-11-18 18:10:15 +0900

    feat: update news detail

--- app/_components/Article/index.tsx
+++ app/_components/Article/index.tsx
@@ -1,4 +1,7 @@
+import { formatDate } from '@/app/_libs/formatDate';
 import { Article } from '@/app/_libs/microcms';
+import Sheet from '../Sheet';
+import styles from './index.module.scss';
 
 type Props = {
   data: Article;
@@ -7,10 +10,17 @@ type Props = {
 export default function Article({ data }: Props) {
   return (
     <main>
-      <h1>{data.title}</h1>
-      <div>
-        <p>{data.date}</p>
-      </div>
+      <Sheet>
+        <div className={styles.heading}>
+          <h1>{data.title}</h1>
+          <time>{formatDate(data.date)}</time>
+        </div>
+
+        <div
+          dangerouslySetInnerHTML={{ __html: data.content }}
+          className={styles.body}
+        />
+      </Sheet>
     </main>
   );
 }
